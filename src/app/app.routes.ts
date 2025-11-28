eda1fde7ca9b9447aab4d9b0e9b6c2136a1b251a
jostin-tech <jostink47@gmail.com>
2025-11-28 09:52:46 +0100

    initialisation et creation des composants

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'ngDuka',
+        loadComponent: () => import('./pages/home/home')
+    },
+    {
+        path: 'products/:category',
+        loadComponent: () => import('./pages/products/products')
+    }
+];
